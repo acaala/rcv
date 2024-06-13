f837b79451c873d035a37235426f7f52d2c44f53
Liam Watts <elnewatts@gmail.com>
2024-06-13 21:02:10 +0100

    - Removed converting messages

--- src/webp_converter.rs
+++ src/webp_converter.rs
@@ -2,11 +2,10 @@ use std::{
     ffi::OsStr,
     fs,
     path::{Path, PathBuf},
-    thread,
 };
 
 use anyhow::{anyhow, Error};
-use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
+use indicatif::{ProgressBar, ProgressStyle};
 use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 use webp::Encoder;
 
@@ -15,7 +14,6 @@ pub struct WebpConverter;
 impl WebpConverter {
     pub fn process_image(input_file: &str, output_path: &Path, quality: f32) -> Result<(), Error> {
         let image_path = Path::new(input_file);
-        let file_size = fs::metadata(image_path).unwrap().len();
 
         let img = image::open(image_path)?;
 
@@ -24,23 +22,12 @@ impl WebpConverter {
             OsStr::new("default")
         });
 
-        // println!("Converting {:?}", file_name);
-
         let encoder = Encoder::from_image(&img).map_err(|_| anyhow!("Failed to create encoder"))?;
 
         let webp = encoder.encode(quality);
 
         let output_path = output_path.join(file_name).with_extension("webp");
         fs::write(&output_path, &*webp).unwrap();
-
-        let new_file_size = fs::metadata(output_path).unwrap().len();
-        let percentage_change =
-            ((file_size as f64 - new_file_size as f64) / file_size as f64) * 100_f64;
-        // println!(
-        //     "Saved {:?} KB ({:?}%)",
-        //     (file_size - new_file_size) / 1024,
-        //     percentage_change as u64
-        // );
 
         Ok(())
     }
